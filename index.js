f0a4b3b4d05da098d68e6a6beec00428caa04f07
theo <teddyhoover@gmail.com>
2021-04-02 11:10:11 -0500

    commit tree 4a13ff10b19d73dabeae8532ccf9c35d16d115fb

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+class Polygon {
+    constructor(sideArray){
+        this.sideArray = sideArray
+    }
+
+    get countSides() {
+        return this.sideArray.length
+    }
+
+    get perimeter() {
+        return this.sideArray.reduce(function(side, memo) {
+            return side + memo
+        })
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        if (this.sideArray[0] + this.sideArray[1] < this.sideArray[2]) {
+            return false
+        } else if (this.sideArray[1] + this.sideArray[2] < this.sideArray[0]) {
+            return false
+        } else if (this.sideArray[2] + this.sideArray[0] < this.sideArray[1]) {
+            return false
+        } else {
+            return true
+        } 
+    }
+}
+
+class Square extends Triangle {
+    get isValid() {
+        if (this.sideArray[0] == this.sideArray[1] && this.sideArray[0] == this.sideArray[2] && this.sideArray[0] == this.sideArray[3]) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    get area() {
+        return this.sideArray[0]*this.sideArray[0]
+    }
+}
